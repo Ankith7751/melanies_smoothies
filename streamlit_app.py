72e055f3e7438c16a883b403f85ec51b034226c1
Ankith7751 <156094281+Ankith7751@users.noreply.github.com>
2024-07-09 19:37:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe,use_container_width=True)
 st.stop()
-#editable_df = st.data_editor(my_dataframe)
+editable_df = st.data_editor(my_dataframe)
 
 
 # Display data as a dataframe
